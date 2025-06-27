2cf56f79fe2ddf39b25a55a2815a902a883355d5
Muhammad Ahsan Yudhistira <142039237+yoosch@users.noreply.github.com>
2025-06-27 11:20:47 +0700

    vercel 2

--- api/index.js
+++ api/index.js
@@ -1,8 +1,33 @@
+const dotenv = require("dotenv");  
+dotenv.config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const menfessRoute = require("../routes/menfessRoute");
+const e = require("express");
+const errorHandler = require("../middlewares/errorHandler");
 const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// middleware
+app.use(express.json());
 
-module.exports = app;
+//routes
+app.use("/api", menfessRoute);
+
+app.get('*', function (req, res) {
+  res.send('index.html')
+})
+
+app.get("/", (req, res) => {
+    res.send("server is running");    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
